d252c9407d0123ae403f9d8711d982efda9d1a43
Krrishiv <krrishiv.kohli06.com>
2025-01-31 22:09:09 -0500

    maps

--- static/scripts/game.js
+++ static/scripts/game.js
@@ -25,6 +25,7 @@ function preload ()
     this.load.image('blackwall', 'static/map/blackwall.png');
     this.load.image('floor', 'static/map/floor.png');
     this.load.image('wall', 'static/map/wall.png');
+    this.load.tilemapTiledJSON('wallCollider', 'static/map/wallscollide.tsj');
     this.load.image('furnitures', 'static/map/furnitures.png');
     this.load.image('behind_player', 'static/map/behind_player.png');
     this.load.spritesheet('Chef1Atlas', 'static/characters/Chef1Atlas.png',{ frameWidth: 34, frameHeight: 68 });
@@ -48,7 +49,7 @@ function create ()
     this.add.image(500, 300, 'blackwall');
     this.add.image(500, 300, 'floor');
     platforms.create(500, 300, 'wall').refreshBody();
-    this.add.image(500, 300, 'furnitures');
+    platforms.create(500, 300, 'furnitures').refreshBody().setImmovable(true);
     this.add.image(500, 300, 'behind_player');
 
     cursors = this.input.keyboard.createCursorKeys();
@@ -59,38 +60,37 @@ function create ()
 
     player = this.physics.add.sprite(450, 350, 'Chef1Atlas');
     player2 = this.physics.add.sprite(350, 350, 'Chef2Atlas');
+
+    
     player.setCollideWorldBounds(true);
+    this.physics.add.collider(player, platforms);
 
     this.add.image(500, 300, 'on_top');
+
     //chef1 animation
     this.anims.create({
         key: 'left1',
         frames: this.anims.generateFrameNumbers('Chef1Atlas', { start: 99, end: 103 }),
-        frameRate: 7,
+        frameRate: 10,
         repeat: -1
     });
 
     this.anims.create({
-        key: 'turn1',
-        frames: [ { key: 'Chef1Atlas', frame: 4 } ],
-        frameRate: 20
-    });
-
-    this.anims.create({
         key: 'right1',
-        frames: this.anims.generateFrameNumbers('Chef1Atlas', { start: 5, end: 8 }),
+        frames: this.anims.generateFrameNumbers('Chef1Atlas', { start: 123, end: 126}),
         frameRate: 10,
         repeat: -1
     });
     this.anims.create({
         key: 'up1',
-        frames: this.anims.generateFrameNumbers('Chef2Atlas', { start: 0, end: 3 }),
+        frames: this.anims.generateFrameNumbers('Chef1Atlas', { start: 127, end: 132 }),
         frameRate: 10,
+        repeat: -1
         
     });
     this.anims.create({
         key: 'down1',
-        frames: this.anims.generateFrameNumbers('Chef1Atlas', { start: 5, end: 8 }),
+        frames: this.anims.generateFrameNumbers('Chef1Atlas', { start: 93, end: 98 }),
         frameRate: 10,
         repeat: -1
     });
@@ -135,23 +135,31 @@ function update ()
 {
     
     if (cursors.left.isDown)
-    {
-        player.setVelocityX(-75);
-        player.anims.play('left1', true);
-    }
-    else if (cursors.right.isDown)
-    {
-        console.log('right');
-    }
-
-    else if (cursors.up.isDown)
-    {
-        console.log('up');
-    }
-    else if (cursors.down.isDown)
-    {
-        console.log('down');
-    }
+        {
+            player.setVelocityY(0);
+            player.setVelocityX(-75);
+            player.anims.play('left1', true);
+        }
+        else if (cursors.right.isDown)
+        {
+            player.setVelocityY(0);
+            player.setVelocityX(75);
+            player.anims.play('right1', true);
+        }
+    
+        else if (cursors.up.isDown)
+        {
+            player.setVelocityX(0);
+            player.setVelocityY(-75);
+            player.anims.play('up1', true);
+        }
+        else if (cursors.down.isDown)
+        {
+            player.setVelocityX(0);
+            player.setVelocityY(75);
+            player.anims.play('down1', true);
+        }
+    
 
     if(keyA.isDown){
         console.log('A');
@@ -165,10 +173,5 @@ function update ()
     else if(keyW.isDown){
         console.log('W');
     }
-
-    this.physics.world.overlap(player, platforms, function(player, platform) {
-        // Set high horizontal friction when player is on platform
-        player.setVelocityX(0); 
-    });
         
 }
